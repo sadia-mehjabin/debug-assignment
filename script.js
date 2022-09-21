774c8c3e5cb0cbdb0a81e4145760e70554df40f2
sadia-mehjabin <jhidnicraft@gmail.com>
2022-09-21 22:49:04 +0600

    miliseconds coverts to seconds

--- script.js
+++ script.js
@@ -134,5 +134,5 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${parseInt(startTime ? timeSpent : 0)} seconds`;
 }, 1000);
